fd9d36d55d4fbc91b8807d95bd6e75325aa3765a
Kair0 <kyleangelodapitan@gmail.com>
2022-05-18 23:11:49 +0800

    added a code for logo generation

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void logo() {
+/**
+ * This is a FigLET Font Generated through the use of https://patorjk.com/software/taag/#p=display&v=2&c=c&f=Cybermedium&t=J%26K's%20Electronics.
+ * Modifications with the text were made in order to allow printing in console.
+ *     _ _  _ . ____    ____ _    ____ ____ ___ ____ ____ _  _ _ ____ ____ 
+ *     | |_/  ' [__     |___ |    |___ |     |  |__/ |  | |\ | | |    [__  
+ *    _| | \_   ___]    |___ |___ |___ |___  |  |  \ |__| | \| | |___ ___] 
+ *                                                                         
+ */
+	printf("     _ _  _ . ____    ____ _    ____ ____ ___ ____ ____ _  _ _ ____ ____\n");
+	printf("     | |_/  ' [__     |___ |    |___ |     |  |__/ |  | |\\ | | |    [__ \n");
+	printf("    _| | \\_   ___]    |___ |___ |___ |___  |  |  \\ |__| | \\| | |___ ___] \n");
+}
+	
 int main() {
-	printf("Hello World");
+	logo();
 	return 0;
 }
