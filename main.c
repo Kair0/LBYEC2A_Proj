91eb7870c26c7bbad371e81c590491afcc9f28c0
Kair0 <66659062+Kair0@users.noreply.github.com>
2022-06-03 18:34:25 +0800

    Added menu printing

--- main.c
+++ main.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "menu.h"
 
 
 void logo() {
 /**
  * This is a FigLET Font Generated through the use of https://patorjk.com/software/taag/#p=display&v=2&c=c&f=Cybermedium&t=J%26K's%20Electronics.
  * Modifications with the text were made in order to allow printing in console.
- *     _ _  _ . ____    ____ _    ____ ____ ___ ____ ____ _  _ _ ____ ____ 
- *     | |_/  ' [__     |___ |    |___ |     |  |__/ |  | |\ | | |    [__  
- *    _| | \_   ___]    |___ |___ |___ |___  |  |  \ |__| | \| | |___ ___] 
- *                                                                         
  */
-	printf("     _ _  _ . ____    ____ _    ____ ____ ___ ____ ____ _  _ _ ____ ____\n");
-	printf("     | |_/  ' [__     |___ |    |___ |     |  |__/ |  | |\\ | | |    [__ \n");
-	printf("    _| | \\_   ___]    |___ |___ |___ |___  |  |  \\ |__| | \\| | |___ ___] \n");
+	printf("  _ _  _ . ____    ____ _    ____ ____ ___ ____ ____ _  _ _ ____ ____\n");
+	printf("  | |_/  ' [__     |___ |    |___ |     |  |__/ |  | |\\ | | |    [__ \n");
+	printf(" _| | \\_   ___]    |___ |___ |___ |___  |  |  \\ |__| | \\| | |___ ___] \n\n");
 }
 	
 int main() {
 	logo();
+	displayMenu();
 	return 0;
 }
---
+++ menu.c
@@ -0,0 +1,48 @@
+#include "menu.h"
+
+//itemCode,itemName,itemPrice
+char menu[][3][40] = {
+	{"001","Banana JPhone 13", "PHP 44 990.00"},
+	{"002","Samsing Milkyway 20","PHP 27 990.00"},
+	{"003","Highway P50","PHP 69 999.00"},
+	{"004","GG 55in Television LED Display","PHP 91 256.72"},
+	{"005","Samsing 55in Television OLED Display","PHP 115 320.18"},
+	{"006","Banana JWatch Series 6","PHP 49 990.00"},
+	{"007","Samsing Milkyway Watch 4","PHP 14 990.00"},
+	{"008","DaMianBao Note 10","PHP 7 938.42"},
+	{"009","SleepStation 5 Pro","PHP 27 607.77"},
+	{"010","SpaghettiBook Air","PHP 54 990.00"},
+	{"011","LandWall Portable 1TB Hard Drive","PHP 2 620.25"},
+	{"012","Ybox  Series Y","PHP 26 156.83"},
+	{"013","Waterpods Pro","PHP 7 690.00"},
+	{"014","Samsing Earbuds Pro","PHP 9 990.00"},
+	{"015","SUSA Vivabook Pro 16x","PHP 88 999.00"},
+	{"016","Boss Noise Cancelling Headphones 700","PHP 19 856.76"},
+	{"017","LBJ Flip 3 Portable Bluetooth Speaker","PHP 9 902.18"},
+	{"018","Sory Camera Alpha 9","PHP 149 999.00"},
+	{"019","GonePro Villain 10","PHP 59 380.00"},
+	{"020","Bipolaroid Camera Go","PHP 15,250.67"}
+};
+
+//prints the item code, menu and the price
+void displayMenu() {
+	int row, column;
+	for (row=0; row<20; row++) {
+		for (column=0; column<3; column++) {
+			//checks if column index is at itemName
+			if(column == 1) {
+				//fills up the string with white spaces for layout purposes
+				char itemName[40] = "";
+				strcat(itemName,menu[row][column]);
+				for(int i = 0; strlen(itemName) < 40; i++) {
+					strcat(itemName," ");
+				}
+				printf("%s\t", itemName);
+			} else {
+				printf("%s\t", menu[row][column]);
+			}	
+		}
+		printf("\n");
+	}
+}
+
